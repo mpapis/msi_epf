99e597a57ba6a8f8bb887de8459d6fa79c649631
Michal Papis <mpapis@gmail.com>
2013-07-20 23:22:06 +0200

    reorganize device/leds

--- lib/msi_epf/device/leds.rb
+++ lib/msi_epf/device/leds.rb
@@ -10,6 +10,16 @@ module MsiEpf
         :front => 0x04,
       }
 
+      def leds=(leds = [])
+        value_to_leds run_control_request(0x01, 0x02, 0x30, leds_to_value(leds))
+      end
+
+      def leds
+        raise "do not know how to do that :("
+      end
+
+    private
+
       def leds_to_value(leds = [])
         unsupported_leds = leds.reject{|led| SUPPORTED_LEDS.keys.include?(led)}
         unless unsupported_leds.empty?
@@ -24,14 +34,6 @@ module MsiEpf
           raise "unsupported led value #{SUPPORTED_LEDS.values.inject(&:|) ^ value}"
         end
         SUPPORTED_LEDS.map{|led, val| value & val == val ? led : nil}.compact
-      end
-
-      def leds=(leds = [])
-        value_to_leds run_control_request(0x01, 0x02, 0x30, leds_to_value(leds))
-      end
-
-      def leds_get
-        raise "do not know how to do that :("
       end
 
     end
